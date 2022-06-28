1c94e2d3b11915d0162c1eadd24ff0d3fa26ac3e
Christina Wu <xchristinawu@gmail.com>
2022-06-27 17:07:40 -0700

    refactor: index.js + index.html

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
-let valueEl = document.getElementById("value");
-let metersFeetEl = document.getElementById("meters-feet");
-let litersGallonsEl = document.getElementById("liters-gallons")
-let kilosPoundsEl = document.getElementById("kilos-pounds")
+const valueEl = document.getElementById("value");
+const inputEl = document.getElementById("input-value");
+const appEl = document.getElementById("app");
+const convertEl = document.getElementById("convert-button");
+const metersFeetEl = document.getElementById("meters-feet");
+const litersGallonsEl = document.getElementById("liters-gallons");
+const kilosPoundsEl = document.getElementById("kilos-pounds");
+
+function convertLength() {
+    const feet = (Math.round((number * 3.281) * 100) / 100).toFixed(2);
+    const meters = (Math.round((number / 3.281) * 100) / 100).toFixed(2);
+    metersFeetEl.textContent = `${number} meters = ${feet} feet | ${number} feet = ${meters} meters`;
+}
+
+function convertVolume() {
+    const gallons = (Math.round((number / 3.785) * 100) / 100).toFixed(2);
+    const liters = (Math.round((number * 3.785) * 100) / 100).toFixed(2);
+    litersGallonsEl.textContent = `${number} liters = ${gallons} gallons | ${number} gallons = ${liters} liters`;
+}
+
+function convertMass() {
+    const pounds = (Math.round((number * 2.205) * 100) / 100).toFixed(2);
+    const kilos = (Math.round((number / 2.205) * 100) / 100).toFixed(2);
+    kilosPoundsEl.textContent = `${number} kilos = ${pounds} pounds | ${number} pounds = ${kilos} kilos`;
+}
+
+function calculateInput() {
+    let inputValue = inputEl.value;
+    if (Number(inputValue)) {
+        valueEl.textContent = inputValue;
+        number = inputValue;
+        convertLength();
+        convertVolume();
+        convertMass();
+        inputValue = "";
+    } else {
+        inputEl.value = "positive numbers only";
+    }
+}
+
+function clearText() {
+    inputEl.value = "";
+}
+
+function pageLoad() {
+    convertLength();
+    convertVolume();
+    convertMass();
+}
 
 let number = 20;
 valueEl.textContent = number;
 
-function convertLength() {
-    let feet = (Math.round((number * 3.281) * 100) / 100).toFixed(2);
-    let meters = (Math.round((number / 3.281) * 100) / 100).toFixed(2);
-    metersFeetEl.textContent = number + " meters = " + feet + " feet | " + number + " feet = " + meters + " meters"
-}
+inputEl.addEventListener("click", clearText);
+convertEl.addEventListener("click",calculateInput);
+app.onload = function() {pageLoad()};
 
-function convertVolume() {
-    let gallons = (Math.round((number / 3.785) * 100) / 100).toFixed(2);
-    let liters = (Math.round((number * 3.785) * 100) / 100).toFixed(2);
-    litersGallonsEl.textContent = number + " liters = " + gallons + " gallons | " + number + " gallons = " + liters + " liters"
-}
 
-function convertMass() {
-    let pounds = (Math.round((number * 2.205) * 100) / 100).toFixed(2);
-    let kilos = (Math.round((number / 2.205) * 100) / 100).toFixed(2);
-    kilosPoundsEl.textContent = number + " kilos = " + pounds + " pounds | " + number + " pounds = " + kilos + " kilos"
-}
-
-function calculateInput() {
-    let inputValue = document.getElementById("input-value").value;
-    valueEl.textContent = inputValue;
-    number = inputValue;
-    convertLength();
-    convertVolume();
-    convertMass();
-    document.getElementById("input-value").value = '';
-}
-
-function clearText() {
-    document.getElementById("input-value").value = ""
-}
